a3cbca71723cd1f868066a41a32aaf76f6103f04
hcj11 <2572366930@qq.com>
2022-07-05 14:34:04 +0800

    loadBalancer client  update the  hostname of eureka

--- spring-cloud-all/config-client/src/test/java/client/loadBalancer/LoadBalancerClient.java
+++ spring-cloud-all/config-client/src/test/java/client/loadBalancer/LoadBalancerClient.java
@@ -9,6 +9,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
@@ -29,26 +31,26 @@ public class LoadBalancerClient {
     StandardEnvironment environment;
     @Autowired
     RestTemplate restTemplate;
-
+//    @Test
+    //   LoadBalancerClientFactory loadBalancerClientFactory = new LoadBalancerClientFactory();
+    //        loadBalancerClientFactory.getInstance("",ReactorServiceInstanceLoadBalancer.class);
     @Test
     public void loadBalancerClientTest(){
         print(context);
         String eureka = environment.getProperty("eureka");
         Assertions.assertEquals(eureka,"172.168.1.73:8761");
-        // appname = peer1,peer2,peer3
-        // getInstance ->gateway -> local
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+//        Assertions.assertEquals(name,"config-client");
+        // LoadBalancerClientFactory
+
+
         String uri= "http://myservice1/get";
         String forObject = restTemplate.getForObject(uri, String.class);
         Assertions.assertEquals(forObject,"hcj");
 
-        uri= "http://peer1/get";
+        uri= "http://peer2/actuator/env";
         String exception = restTemplate.getForObject(uri, String.class);
-//
         log.info("{}",exception);
-        HttpClientErrorException exception2 = restTemplate.getForObject(uri, HttpClientErrorException.class);
-        // 404 : [{"timestamp":"2022-07-05T04:03:18.453+0000","status":404,"error":"Not Found","message":"No message available","path":"/get"}]
-        Assertions.assertNotNull(exception2);
-//        Assertions.assertEquals(exception.get);
 
 
     }
