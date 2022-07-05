08d33f42cc36870e48dc248ba45327dedd514a6a
hcj11 <2572366930@qq.com>
2022-07-05 10:40:17 +0800

    loadBalancer client

--- spring-cloud-all/gateway-server/src/main/java/com/SpringBootGatewayApps.java
+++ spring-cloud-all/gateway-server/src/main/java/com/SpringBootGatewayApps.java
@@ -1,14 +1,23 @@
 package com;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import utils.Utils;
 
 @SpringBootApplication
 public class SpringBootGatewayApps {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
